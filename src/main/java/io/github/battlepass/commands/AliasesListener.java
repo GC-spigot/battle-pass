98cc7cc739a59cbeb606a436a65ebcdc7dd49391
Zak Shearman <shearmanzak@gmail.com>
2020-08-05 08:57:06 +0100

    Refactor & clean

--- src/main/java/io/github/battlepass/commands/AliasesListener.java
+++ src/main/java/io/github/battlepass/commands/AliasesListener.java
@@ -11,10 +11,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class AliasesListener implements Listener {
-    private final List<String> lqAliases;
+    private final List<String> bpAliases;
 
     public AliasesListener(BattlePlugin plugin) {
-        this.lqAliases = plugin.getConfig("settings").list("battle-pass-aliases");
+        this.bpAliases = plugin.getConfig("settings").list("battle-pass-aliases");
     }
 
     @EventHandler
@@ -22,7 +22,7 @@ public class AliasesListener implements Listener {
         Player player = event.getPlayer();
         String strippedCommand = event.getMessage().replace("/", "");
         String[] arguments = strippedCommand.split(" ");
-        for (String alias : this.lqAliases) {
+        for (String alias : this.bpAliases) {
             if (arguments[0].equalsIgnoreCase(alias)) {
                 event.setCancelled(true);
                 String preparedArguments = arguments.length > 1 ? Arrays
