217b731b1a4b98327a8303afdf090a74bd8dfba8
Zak Shearman <shearmanzak@gmail.com>
2020-08-19 18:42:15 +0100

    Customize menu code in runtime

--- src/main/java/io/github/battlepass/menus/MenuFactory.java
+++ src/main/java/io/github/battlepass/menus/MenuFactory.java
@@ -1,5 +1,6 @@
 package io.github.battlepass.menus;
 
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import io.github.battlepass.BattlePlugin;
 import io.github.battlepass.menus.menus.DailyQuestsMenu;
@@ -9,31 +10,27 @@ import io.github.battlepass.menus.menus.rewards.DefaultRewardsMenu;
 import me.hyfe.simplespigot.menu.Menu;
 import org.bukkit.entity.Player;
 
+import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class MenuFactory {
     private final BattlePlugin plugin;
     private final Set<UUID> insideMenu = Sets.newHashSet();
+    private final Map<Set<String>, Function<Player, Menu>> menus = Maps.newHashMap();
 
     public MenuFactory(BattlePlugin plugin) {
         this.plugin = plugin;
+        this.putDefaults();
     }
 
     public Menu createMenu(String menuName, Player player) {
-        switch (menuName) {
-            case "portal":
-                return this.initiateMenu(player, () -> new PortalMenu(this.plugin, this.plugin.getConfig("portal-menu"), player));
-            case "daily":
-            case "daily-quests":
-                return this.initiateMenu(player, () -> new DailyQuestsMenu(this.plugin, this.plugin.getConfig("daily-quests-menu"), player));
-            case "rewards":
-                return this.initiateMenu(player, () -> new DefaultRewardsMenu(this.plugin, this.plugin.getConfig("rewards-menu"), player));
-            case "missions":
-            case "quests":
-            case "quest-overview":
-                return this.initiateMenu(player, () -> new QuestOverviewMenu(this.plugin, this.plugin.getConfig("quest-overview-menu"), player));
+        for (Map.Entry<Set<String>, Function<Player, Menu>> entry : this.menus.entrySet()) {
+            if (entry.getKey().contains(menuName)) {
+                return this.initiateMenu(player, () -> entry.getValue().apply(player));
+            }
         }
         return null;
     }
@@ -42,10 +39,26 @@ public class MenuFactory {
         return this.insideMenu;
     }
 
+    /**
+     * Returns a mutable map so you can technically make your own menu. See the source for examples of how to do this
+     *
+     * @return A map of the names of the menu to a function giving you the Player where you return your Menu.
+     */
+    public Map<Set<String>, Function<Player, Menu>> getMenus() {
+        return this.menus;
+    }
+
     private Menu initiateMenu(Player player, Supplier<Menu> supplier) {
         this.insideMenu.add(player.getUniqueId());
         Menu menu = supplier.get();
         menu.setCloseAction(() -> this.insideMenu.remove(player.getUniqueId()));
         return menu;
+    }
+
+    private void putDefaults() {
+        this.menus.put(Sets.newHashSet("portal"), player -> new PortalMenu(this.plugin, this.plugin.getConfig("portal-menu"), player));
+        this.menus.put(Sets.newHashSet("daily", "daily-quests"), player -> new DailyQuestsMenu(this.plugin, this.plugin.getConfig("portal-menu"), player));
+        this.menus.put(Sets.newHashSet("rewards"), player -> new DefaultRewardsMenu(this.plugin, this.plugin.getConfig("portal-menu"), player));
+        this.menus.put(Sets.newHashSet("missions", "quests", "quest-overview"), player -> new QuestOverviewMenu(this.plugin, this.plugin.getConfig("portal-menu"), player));
     }
 }
--- src/main/java/io/github/battlepass/menus/menus/WeekMenu.java
+++ src/main/java/io/github/battlepass/menus/menus/WeekMenu.java
@@ -58,9 +58,9 @@ public class WeekMenu extends PageableConfigMenu<Quest> {
                     .set("progress_bar", this.getProgressBar(this.questController.getQuestProgress(this.user, quest), quest.getRequiredProgress()))))
                     .build();
         } catch (Exception e) {
-            Bukkit.getLogger().log(Level.INFO, "Quest: ".concat(String.valueOf(quest)));
-            Bukkit.getLogger().log(Level.INFO, "Error whilst building menu item for quest: " + quest.getId() + " category " + quest.getCategoryId() + " name " + quest.getName());
-            Bukkit.getLogger().log(Level.INFO, "Quest Item: ".concat(String.valueOf(quest.getItemStack())));
+            Bukkit.getLogger().log(Level.WARNING, "Quest: ".concat(String.valueOf(quest)));
+            Bukkit.getLogger().log(Level.WARNING, "Error whilst building menu item for quest: " + quest.getId() + " category " + quest.getCategoryId() + " name " + quest.getName());
+            Bukkit.getLogger().log(Level.WARNING, "Quest Item: ".concat(String.valueOf(quest.getItemStack())));
             e.printStackTrace();
             return MenuItem.builderOf(new ItemStack(Material.BARRIER)).build();
         }
