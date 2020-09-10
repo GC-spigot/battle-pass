395093574989449dda708b86ab6d0313ff8f640b
Yasin <20637793+bertek41@users.noreply.github.com>
2020-09-10 11:47:07 +0300

    Harvest quest

--- src/main/java/io/github/battlepass/quests/quests/internal/BlockBreakQuest.java
+++ src/main/java/io/github/battlepass/quests/quests/internal/BlockBreakQuest.java
@@ -3,6 +3,7 @@ package io.github.battlepass.quests.quests.internal;
 import io.github.battlepass.BattlePlugin;
 import io.github.battlepass.quests.QuestExecutor;
 import org.bukkit.block.Block;
+import org.bukkit.block.data.Ageable;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -20,5 +21,9 @@ public class BlockBreakQuest extends QuestExecutor {
         Block block = event.getBlock();
 
         super.execute("block-break", player, result -> result.root(block), replacer -> replacer.set("block", block.getType()));
+        if(block.getBlockData() instanceof Ageable) {
+        	Ageable ageable = (Ageable) block.getBlockData();
+        	if(ageable.getAge() == ageable.getMaximumAge()) super.execute("harvest", player, result -> result.root(block));
+        }
     }
 }
