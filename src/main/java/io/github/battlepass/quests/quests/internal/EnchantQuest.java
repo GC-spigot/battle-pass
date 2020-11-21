d89438b81c693e0443a13d97b4ad809266c5c13f
Zak Shearman <shearmanzak@gmail.com>
2020-11-21 16:53:59 +0000

    enchant-anvil quest

--- src/main/java/io/github/battlepass/quests/quests/internal/EnchantQuest.java
+++ src/main/java/io/github/battlepass/quests/quests/internal/EnchantQuest.java
@@ -1,15 +1,24 @@
 package io.github.battlepass.quests.quests.internal;
 
 import io.github.battlepass.BattlePlugin;
+import io.github.battlepass.objects.quests.variable.QuestResult;
 import io.github.battlepass.quests.QuestExecutor;
+import me.hyfe.simplespigot.version.MultiMaterial;
 import me.hyfe.simplespigot.version.ServerVersion;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.enchantment.EnchantItemEvent;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.AnvilInventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 
 import java.util.Iterator;
+import java.util.Map;
+import java.util.function.UnaryOperator;
 
 public class EnchantQuest extends QuestExecutor {
 
@@ -30,8 +39,7 @@ public class EnchantQuest extends QuestExecutor {
             enchantLevel = 0;
         } else if (ServerVersion.isOver_V1_12()) {
             Enchantment enchant = iterator.next();
-            String possibleName = enchant.getKey().getNamespace();
-            enchantment = possibleName.startsWith("minecraft:") ? possibleName.split("minecraft:")[1] : possibleName;
+            enchantment = this.getEnchantName(enchant);
             enchantLevel = event.getEnchantsToAdd().get(enchant);
         } else {
             Enchantment enchant = iterator.next();
@@ -39,11 +47,45 @@ public class EnchantQuest extends QuestExecutor {
             enchantment = enchant.getName();
         }
 
-        this.execute("enchant", player, result -> {
+        UnaryOperator<QuestResult> resultOperator = result -> {
             result.subRoot("cost", String.valueOf(levelCost));
             result.subRoot("level", String.valueOf(enchantLevel));
             result.subRoot(item);
             return result.root(enchantment);
-        });
+        };
+        this.execute("enchant", player, resultOperator);
+        this.execute("enchant-all", player, resultOperator);
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onAnvilEnchant(InventoryClickEvent event) {
+        if (!(event.getWhoClicked() instanceof Player) || !(event.getInventory() instanceof AnvilInventory) ||
+                event.getSlotType() != InventoryType.SlotType.RESULT || event.getClick() == ClickType.MIDDLE) {
+            return;
+        }
+
+        AnvilInventory inventory = (AnvilInventory) event.getInventory();
+        ItemStack book = inventory.getItem(1);
+        if (book == null || book.getType() != MultiMaterial.ENCHANTED_BOOK.getMaterial()) {
+            return;
+        }
+        for (Map.Entry<Enchantment, Integer> entry : ((EnchantmentStorageMeta) book.getItemMeta()).getStoredEnchants().entrySet()) {
+            String enchantName = this.getEnchantName(entry.getKey());
+            UnaryOperator<QuestResult> resultOperator = result -> {
+                result.subRoot("level", String.valueOf(entry.getValue()));
+                return result.root(enchantName);
+            };
+
+            this.execute("enchant-anvil", ((Player) event.getWhoClicked()).getPlayer(), resultOperator);
+            this.execute("enchant-all", ((Player) event.getWhoClicked()).getPlayer(), resultOperator);
+        }
+    }
+
+    private String getEnchantName(Enchantment enchantment) {
+        if (ServerVersion.isOver_V1_12()) {
+            String possibleName = enchantment.getKey().getNamespace();
+            return possibleName.startsWith("minecraft:") ? possibleName.split("minecraft:")[1] : possibleName;
+        }
+        return enchantment.getName();
     }
 }
