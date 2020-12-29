1fd192972d237219e8956d0a4d3f0c98377dda7a
Alex <72355308+alexwith@users.noreply.github.com>
2020-12-29 13:24:32 +0100

    player placeholder to actions

--- src/main/java/io/github/battlepass/api/BattlePassApi.java
+++ src/main/java/io/github/battlepass/api/BattlePassApi.java
@@ -155,7 +155,9 @@ public class BattlePassApi {
                 this.reward(user, tier, false);
                 Player player = Bukkit.getPlayer(user.getUuid());
                 if (player != null) {
-                    Action.executeSimple(player, this.passLoader.getPassTypes().get(user.getPassId()).getTierUpActions(), this.plugin, new Replacer().set("tier", tier));
+                    Action.executeSimple(player, this.passLoader.getPassTypes().get(user.getPassId()).getTierUpActions(), this.plugin, new Replacer()
+                            .set("tier", tier)
+                            .set("player", player.getName()));
                 }
             } else {
                 break;
