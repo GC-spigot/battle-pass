a67a4639a01cca1b742db2cc2f85cd530769e179
Zak <shearmanzak@gmail.com>
2021-01-31 20:15:48 +0000

    Check entity is not armour stand with wildstacker

--- src/main/java/io/github/battlepass/quests/quests/internal/KillMobQuest.java
+++ src/main/java/io/github/battlepass/quests/quests/internal/KillMobQuest.java
@@ -4,6 +4,7 @@ import com.bgsoftware.wildstacker.api.WildStackerAPI;
 import io.github.battlepass.BattlePlugin;
 import io.github.battlepass.quests.QuestExecutor;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -24,7 +25,7 @@ public class KillMobQuest extends QuestExecutor {
         }
         String stringEntity = entity.getType().toString().replace("Craft", "");
         int entityAmount = 1;
-        if (Bukkit.getPluginManager().isPluginEnabled("WildStacker")) {
+        if (event.getEntity().getType() != EntityType.ARMOR_STAND && Bukkit.getPluginManager().isPluginEnabled("WildStacker")) {
             entityAmount = WildStackerAPI.getEntityAmount(event.getEntity());
         }
         this.execute("kill-mob", player, entityAmount, result -> result.root(stringEntity), replacer -> replacer.set("entity", stringEntity));
